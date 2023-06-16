e0fc27503acd2654f9f6b3ed5e8d154539138533
Neves <victorneveshs@gmail.com>
2023-06-16 16:27:47 -0300

    specialist details

--- src/main/java/com/demand/api/controller/SpecialistPostController.java
+++ src/main/java/com/demand/api/controller/SpecialistPostController.java
@@ -7,10 +7,9 @@ import com.demand.api.service.SpecialistPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/specialist")
@@ -27,4 +26,24 @@ public class SpecialistPostController {
         specialistPostModel.setContact(specialistPostDTO.getContact());
         return ResponseEntity.status(HttpStatus.CREATED).body(specialistPostService.save(specialistPostModel));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getSpecialist(@PathVariable(value = "id") Long id){
+        Optional<SpecialistPostModel> specialistPostModelOptional = specialistPostService.findById(id);
+        if (!specialistPostModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer Spot not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(specialistPostModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteSpecialist(@PathVariable(value = "id") Long id){
+        Optional<SpecialistPostModel> specialistPostModelOptional = specialistPostService.findById(id);
+        if (!specialistPostModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found.");
+        }
+        specialistPostService.delete(specialistPostModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Customer deleted successfully.");
+    }
+
 }
